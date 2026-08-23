5b264f614df2b7586c69743ca0f9cebc2e2512a7
kiiiiirdffg <wsdfthjjj@atomicmail.io>
2026-08-23 16:35:56 +0330

    Delete start.sh

--- start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-pip install --upgrade pip setuptools wheel
-pip install -r requirements.txt
-curl -sL https://github.com/Gozargah/Marzban-scripts/raw/master/install_latest_xray.sh | bash
-cp -n .env.example .env
-alembic upgrade head
-python3 main.py
